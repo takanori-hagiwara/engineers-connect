d6c0e67efd974cf50d4c5a6748ce481c34bc365f
takanori-hagiwara <allhappyhagi@outlook.com>
2021-03-10 18:15:35 +0900

    新規登録、ログイン機能実装
    ビューの編集はしていない

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_070417) do
+ActiveRecord::Schema.define(version: 2021_03_10_090022) do
+
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "overview", null: false
+    t.text "git_url", null: false
+    t.text "trello_url", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_03_10_070417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
